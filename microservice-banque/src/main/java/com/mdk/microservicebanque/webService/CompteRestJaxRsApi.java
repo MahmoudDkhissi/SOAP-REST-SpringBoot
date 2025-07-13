e010a54e5b17f1ad1054e52ecda57c7ec48ab983
Mahmoud Dkhissi <mahmouddkhissi68@gmail.com>
2025-07-13 15:38:32 +0200

    REST avec jaxRS

---
+++ microservice-banque/src/main/java/com/mdk/microservicebanque/webService/CompteRestJaxRsApi.java
@@ -0,0 +1,49 @@
+package com.mdk.microservicebanque.webService;
+
+
+import com.mdk.microservicebanque.entities.Compte;
+import com.mdk.microservicebanque.repositories.CompteRepository;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Path("/banque")
+public class CompteRestJaxRsApi {
+
+    CompteRepository compteRepository;
+
+    public CompteRestJaxRsApi(CompteRepository compteRepository) {
+        this.compteRepository = compteRepository;
+    }
+
+    @GET
+    @Path("/comptes")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Compte> compteList() {
+        return compteRepository.findAll();
+    }
+
+    @GET
+    @Path("/comptes/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Compte getCompte(@PathParam("id") Long id) {
+        return compteRepository.getCompteById(id);
+    }
+
+    @POST
+    @Path("/comptes")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Compte save(Compte compte) {
+        return compteRepository.save(compte);
+    }
+
+    @DELETE
+    @Path("/comptes/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void delete(@PathParam("id") Long id) {
+        compteRepository.deleteById(id);
+    }
+}
