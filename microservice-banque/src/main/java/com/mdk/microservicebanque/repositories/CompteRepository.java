1c3b5f56c03ee13f8d414b2c926c4c7ee95ebb0d
Mahmoud Dkhissi <mahmouddkhissi68@gmail.com>
2025-07-16 20:47:47 +0200

    Spring Data Rest Controller

--- microservice-banque/src/main/java/com/mdk/microservicebanque/repositories/CompteRepository.java
+++ microservice-banque/src/main/java/com/mdk/microservicebanque/repositories/CompteRepository.java
@@ -2,7 +2,9 @@ package com.mdk.microservicebanque.repositories;
 
 import com.mdk.microservicebanque.entities.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource
 public interface CompteRepository extends JpaRepository<Compte, Long> {
 
     Compte getCompteById(Long id);
